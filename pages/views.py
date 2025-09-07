d2f6e27a0fdf60bc66a8cf4db91f5c378f885607
SamStent <wuttkefederico@gmail.com>
2025-09-07 11:33:19 +0100

    testing git status

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    print('testeando home')
     return render(request, 'pages/home.html')
+
+
+def test(request):
+    print(request)
+    return render(request, 'pages/test.html')
